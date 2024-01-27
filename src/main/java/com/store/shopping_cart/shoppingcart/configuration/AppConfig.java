b2ea2ed42f8a6d0b164835af499b9c3c6f9cdc34
Alejandro Aragon <aragondru155@gmail.com>
2024-01-26 21:10:56 -0600

    commented configs temporaly

--- src/main/java/com/store/shopping_cart/shoppingcart/configuration/AppConfig.java
+++ src/main/java/com/store/shopping_cart/shoppingcart/configuration/AppConfig.java
@@ -1,12 +1,12 @@
 package com.store.shopping_cart.shoppingcart.configuration;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.authentication.AuthenticationProvider;
+// import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -19,23 +19,23 @@ public class AppConfig {
     //     return username -> UserDetailsImp.build(userRepository.findByUsername(username).get());
     // }
 
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // BCryptPasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+    // @Bean
+    // public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    //     return config.getAuthenticationManager();
+    // }
 
-    @Bean
-    AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    // @Bean
+    // AuthenticationProvider authenticationProvider() {
+    //     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
-        // authProvider.setUserDetailsService(userDetailsService());
-        // authProvider.setPasswordEncoder(passwordEncoder());
+    //     // authProvider.setUserDetailsService(userDetailsService());
+    //     // authProvider.setPasswordEncoder(passwordEncoder());
 
-        return authProvider;
-    }
+    //     return authProvider;
+    // }
 }
